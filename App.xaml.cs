a334a840b46f1392c5dde87cefc21746fdf14311
William_Crow <cruzlopezwilliam030520@gmail.com>
2026-02-12 09:48:21 -0600

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Pages
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
